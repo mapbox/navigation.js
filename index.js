177749a58126165aa1973f608afb33b5597a3ec3
bsudekum <bobby@mapbox.com>
2016-02-12 17:12:09 -0800

    Use distance to see if user completed route

--- index.js
+++ index.js
@@ -2,32 +2,18 @@ var turfPointOnLine = require('turf-point-on-line');
 var turfDistance = require('turf-distance');
 var turfLineDistance = require('turf-line-distance');
 var turfLineSlice = require('turf-line-slice');
+
 module.exports = function(opts) {
     /**
     * Configuration options
-    * @param {object} `units` - either `miles` or `km`. `maxReRouteDistance` - max distance the user can be from the route. `maxSnapToLocation` - max distance to snap user to route.
+    * @param {object} `units` - either `miles` or `km`. `maxReRouteDistance` - max distance the user can be from the route. `maxSnapToLocation` - max distance to snap user to route. `completionDistance` - distance away from end of step that is considered a completion. If this distance is shorter than the step distance, it will be changed to 10ft. `warnUserTime` - number of seconds ahead of maneuver to warn user about maneuver.
     */
     var options = {
         units: opts.units || 'miles',
-        maxReRouteDistance: opts.maxReRouteDistance || 0.03,
-        maxSnapToLocation: opts.maxSnapToLocation || 0.01
-    };
-
-    /**
-     * Given a user location and route, calculates whether re-routing should occur.
-     * @param {object} user point feature representing user location. Must be a valid GeoJSON object.
-     * @param {object} route from [Mapbox directions API](https://www.mapbox.com/developers/api/directions/).
-     * The Mapbox directions API returns an object with up to 2 `routes` on the `route` key. `shouldReRoute` expects of these routes, either the first or second.
-     * @returns {boolean} should user be re-routed.
-     */
-    function shouldReRoute(user, route) {
-        var r = {
-            type: 'Feature',
-            properties: {},
-            geometry: route.geometry
-        };
-        var closestPoint = turfPointOnLine(r, user);
-        return turfDistance(user, closestPoint, options.units) > options.maxReRouteDistance ? true : false;
+        maxReRouteDistance: opts.maxReRouteDistance || 0.0284091, // 150 ft
+        maxSnapToLocation: opts.maxSnapToLocation || 0.0094697,   // 50 ft
+        completionDistance: opts.completionDistance || 0.0094697, // 50 ft
+        warnUserTime: opts.warnUserTime || 30                     // 30 seconds
     };
 
     /**
@@ -35,9 +21,10 @@ module.exports = function(opts) {
      * @param {object} user point feature representing user location. Must be a valid GeoJSON object.
      * @param {object} route from [Mapbox directions API](https://www.mapbox.com/developers/api/directions/).
      * The Mapbox directions API returns an object with up to 2 `routes` on the `route` key. `findNextStep` expects of these routes, either the first or second.
+     * @param {number} userCurrentStep along the route
      * @returns {object} Containing 3 keys: `step`, `distance`, `snapToLocation`. `distance` is the line distance to end of step, `absoluteDistance` is the users absolute distance to the end of the route `snapToLocation` is location along route which is closest to the user.
      */
-    function findNextStep(user, route) {
+    function findNextStep(user, route, userCurrentStep) {
         var previousSlice = 0;
         var currentMax = Infinity;
         var currentStep = {};
@@ -45,7 +32,7 @@ module.exports = function(opts) {
         var stepCoordinates = route.steps;
 
         for (var p = 0; p < routeCoordinates.length; p++) {
-            for (var i = 0; i < stepCoordinates.length; i++) {;
+            for (var i = userCurrentStep; i <= userCurrentStep; i++) {
                 if (arraysEqual(stepCoordinates[i].maneuver.location.coordinates, routeCoordinates[p])) {
                     var slicedSegment = routeCoordinates.slice(previousSlice, p + 1);
                     previousSlice = p;
@@ -65,10 +52,21 @@ module.exports = function(opts) {
 
                         var segmentEndPoint = { type: 'Feature', properties: {}, geometry: { type: 'Point', coordinates: slicedSegment[slicedSegment.length - 1] }};
                         var segmentSlicedToUser = turfLineSlice(user, segmentEndPoint, segmentRoute);
+                        var userDistanceToEndStep = turfLineDistance(segmentSlicedToUser, options.units);
 
-                        currentStep.distance = turfLineDistance(segmentSlicedToUser, options.units);
+                        var segmentDistance = turfLineDistance(segmentRoute, options.units);
+                        var completePercent = userDistanceToEndStep / segmentDistance;
+                        var warnPercent = stepCoordinates[i - 1].duration > options.warnUserTime ? 1 - ((stepCoordinates[i - 1].duration - 30) / stepCoordinates[i - 1].duration) : 0;
+
+                        var stepDistance = options.units === 'miles' ? stepCoordinates[i - 1].distance * 0.000621371 : stepCoordinates[i].distance * 1000;
+                        // If the step distance is less than options.completionDistance, modify it and make it 10 ft
+                        var modeifiedCompletionDistance = stepDistance < options.completionDistance ? 0.00189394 : options.completionDistance;
+                        currentStep.step = userDistanceToEndStep < modeifiedCompletionDistance && i < stepCoordinates.length - 1 ? userCurrentStep + 1 : userCurrentStep; // Don't set next step + 1 if at the end of the route
+
+                        currentStep.distance = userDistanceToEndStep;
+                        currentStep.shouldReRoute = turfDistance(user, closestPoint, options.units) > options.maxReRouteDistance ? true : false;
                         currentStep.absoluteDistance = turfDistance(user, segmentEndPoint, options.units);
-                        currentStep.step = i;
+                        currentStep.alertUser = completePercent < warnPercent ? true : false;
                         currentStep.snapToLocation = distance < opts.maxSnapToLocation ? closestPoint : user;
                     }
                 }
@@ -86,7 +84,6 @@ module.exports = function(opts) {
     }
 
     return {
-        shouldReRoute: shouldReRoute,
         findNextStep: findNextStep
     };
 };
--- tests/debug/index.js
+++ tests/debug/index.js
@@ -1,12 +1,15 @@
 require('mapbox.js');
 require('leaflet-hash');
-var activeTest = 0;
+var request = require('request');
+var debounce = require('debounce');
+
 L.mapbox.accessToken = 'pk.eyJ1IjoiYm9iYnlzdWQiLCJhIjoiTi16MElIUSJ9.Clrqck--7WmHeqqvtFdYig';
 var userLocation = { type: 'Feature', properties: {}, geometry: { type: 'Point', coordinates: [] }};
 var places = document.getElementById('places');
+var routeGeoJSON = L.layerGroup();
 
 // Routes to test
-var downtown = require('../fixtures/route');
+var downtown = require('../fixtures/downtown');
 var loop = require('../fixtures/routeSF');
 var highway = require('../fixtures/highway');
 
@@ -35,20 +38,58 @@ var testCases = [
         name: 'Highway'
     }
 ];
+var activeTest = testCases[0].route.routes[0];
+var currentStep = 1;
 
 var map = L.mapbox.map('map', 'mapbox.streets').setView([39.9432, -75.1433], 14);
 var marker = L.marker([0, 0]).addTo(map);
 L.hash(map);
+routeGeoJSON.addTo(map);
 
 for (var i = 0; i < testCases.length; i++) {
     var div = document.createElement('div');
-    div.innerHTML = '<a href=# data-route=' + i + ' data-name=' + testCases[i].name + ' data-lng=' + testCases[i].center[1] + ' data-lat=' + testCases[i].center[0] + ' data-zoom=' + testCases[i].zoom + '>' + testCases[i].name + '</a>';
+    div.innerHTML = '<a href=# data-route=' + i + ' data-name=' + i + ' data-lng=' + testCases[i].center[1] + ' data-lat=' + testCases[i].center[0] + ' data-zoom=' + testCases[i].zoom + '>' + testCases[i].name + '</a>';
     places.appendChild(div);
     div.addEventListener('click', function(e) {
         map.setView([e.target.dataset.lat, e.target.dataset.lng], e.target.dataset.zoom);
-        activeTest = e.target.dataset.route;
+        activeTest = testCases[parseInt(e.target.dataset.route)].route.routes[0];
+        addToMap(testCases[parseInt(e.target.dataset.route)].route);
     });
+};
 
+addToMap(testCases[0].route);
+
+map.on('mousemove', function(e) {
+    userLocation.geometry.coordinates[0] = e.latlng.lng;
+    userLocation.geometry.coordinates[1] = e.latlng.lat;
+
+    var nextStep = navigation.findNextStep(userLocation, activeTest, currentStep);
+    document.getElementById('reroute').innerHTML = nextStep.shouldReRoute;
+
+    if (nextStep.shouldReRoute) {
+        var last = activeTest.geometry.coordinates[activeTest.geometry.coordinates.length - 1];
+        getRouteDebounced(e.latlng.lng, e.latlng.lat, last[0], last[1], function(err, route) {
+            routeGeoJSON.clearLayers();
+            addToMap(route);
+            activeTest = route.routes[0];
+            currentStep = 1;
+        });
+    }
+
+    if (nextStep.step > currentStep) currentStep = nextStep.step;
+    marker.setLatLng([nextStep.snapToLocation.geometry.coordinates[1], nextStep.snapToLocation.geometry.coordinates[0]]);
+    document.getElementById('step').innerHTML = 'In ' + Math.round(nextStep.distance * 5280) + ' '+ activeTest.steps[nextStep.step].maneuver.instruction;
+});
+
+function getRoute(fromLng, fromLat, toLng, toLat, callback) {
+    request('https://api.mapbox.com/v4/directions/mapbox.driving/' + fromLng + ',' + fromLat + ';' + toLng + ',' + toLat + '.json?alternatives=false&steps=true&access_token=pk.eyJ1IjoiYm9iYnlzdWQiLCJhIjoiTi16MElIUSJ9.Clrqck--7WmHeqqvtFdYig', function(err, res, body) {
+        if (err) return callback(err);
+        if (body && res.statusCode === 200) return callback(null, JSON.parse(body));
+    });
+}
+var getRouteDebounced = debounce(getRoute, 1000);
+
+function addToMap(route) {
     L.geoJson({
         'type': 'FeatureCollection',
         'features': [
@@ -56,34 +97,9 @@ for (var i = 0; i < testCases.length; i++) {
                 'type': 'Feature',
                 'geometry': {
                     'type': 'LineString',
-                    'coordinates': testCases[i].route.routes[0].geometry.coordinates
+                    'coordinates': route.routes[0].geometry.coordinates
                 }
             }
         ]
-    }).addTo(map);
-
-    for (var p = 0; p < testCases[i].route.routes[0].steps.length; p++) {
-        var maneuver = testCases[i].route.routes[0].steps[p].maneuver;
-
-        L.circle([maneuver.location.coordinates[1], maneuver.location.coordinates[0]], 30, {
-            color: 'black',
-            weight: 1,
-            fillColor: 'black'
-        })
-            .bindPopup(maneuver.instruction + '. Step: ' + i)
-            .addTo(map);
-    };
-
-};
-
-map.on('mousemove', function(e) {
-    userLocation.geometry.coordinates[0] = e.latlng.lng;
-    userLocation.geometry.coordinates[1] = e.latlng.lat;
-
-    var shouldReRoute = navigation.shouldReRoute(userLocation, testCases[activeTest].route.routes[0]);
-    document.getElementById('reroute').innerHTML = shouldReRoute;
-
-    var nextStep = navigation.findNextStep(userLocation, testCases[activeTest].route.routes[0]);
-    marker.setLatLng([nextStep.snapToLocation.geometry.coordinates[1], nextStep.snapToLocation.geometry.coordinates[0]]);
-    document.getElementById('step').innerHTML = 'In ' + Math.round(nextStep.distance * 5280) + ' '+ testCases[activeTest].route.routes[0].steps[nextStep.step].maneuver.instruction;
-});
+    }).addTo(routeGeoJSON);
+}
